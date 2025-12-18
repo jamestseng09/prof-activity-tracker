923725a4d10e2b8e73a568e26f900180ae55bbc4
jamestseng09 <james.tseng@twyfp.com>
2025-12-18 17:11:13 +0800

    Update daily_activity.py

--- daily_activity.py
+++ daily_activity.py
@@ -22,13 +22,13 @@ def main():
     gc = gspread.authorize(creds)
 
     sh = gc.open_by_key(os.environ["SHEET_ID"])
-    prof_master = sh.worksheet("PROF_MASTER")
+    raw_data = sh.worksheet("RAW_DATA")
     snapshot_ws = sh.worksheet("SNAPSHOT")
     log_ws = sh.worksheet("DAILY_ACTIVITY_LOG")
 
     today = datetime.date.today().isoformat()
 
-    profs = prof_master.get_all_records()
+    RAW_DATA = raw_data.get_all_records()
     snapshot = {r["prof_id"]: r for r in snapshot_ws.get_all_records()}
 
     log_rows = []
